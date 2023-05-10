9febec40d09dce240064b299163f6dff4ff7b36d
“username” <emailaddr>
2023-05-10 16:44:53 +0530

    Added Routes

--- src/App.js
+++ src/App.js
@@ -1,35 +1,17 @@
 import "./App.css";
-import TourCard from "./components/TourCard";
-import Container from "@mui/material/Container";
-import Grid from "@mui/material/Grid";
+import Home from "./pages/Home";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 import SearchAppBar from "./components/AppBar";
-import cities from "./data.json";
-import { Typography } from "@mui/material";
 
 function App() {
   return (
-    <div className="App">
+    <BrowserRouter>
       <SearchAppBar />
-      <Container sx={{ marginY: 5 }}>
-        {cities.map((city) => (
-          <>
-            <Typography
-              variant="h4"
-              component="h2"
-              marginTop={5}
-              marginBottom={3}
-            >
-              Top {city.name} Tours
-            </Typography>
-            <Grid container spacing={5}>
-              {city.tours.map((tour, index) => (
-                <TourCard tour={tour} key={index} />
-              ))}
-            </Grid>
-          </>
-        ))}
-      </Container>
-    </div>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/:id" element={<Tour />} />
+      </Routes>
+    </BrowserRouter>
   );
 }
 
