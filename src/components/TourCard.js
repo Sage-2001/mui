504bc343421998eed77f04fb8f719b3fb0f0da61
“username” <emailaddr>
2023-05-10 13:58:47 +0530

    Typography Integration with the react code

--- src/components/TourCard.js
+++ src/components/TourCard.js
@@ -1,6 +1,9 @@
 import Paper from "@mui/material/Paper";
 import Grid from "@mui/material/Grid";
+import { Box } from "@mui/material";
 import { Typography } from "@mui/material";
+import { AccessTime } from "@mui/icons-material";
+import Rating from "@mui/material/Rating";
 
 const TourCard = () => {
   return (
@@ -11,9 +14,50 @@ const TourCard = () => {
           alt=""
           className="img"
         />
-        <Typography variant="subtitle1" component="h2">
-          Immerse into the Falls
-        </Typography>
+        <Box paddingX={1}>
+          <Typography variant="subtitle1" component="h2">
+            Immerse into the Falls
+          </Typography>
+          <Box
+            sx={{
+              display: "flex",
+              alignItems: "center",
+            }}
+          >
+            <AccessTime sx={{ width: 12.5 }} />
+            <Typography variant="body2" component="p" marginLeft={0.5}>
+              5 hours
+            </Typography>
+          </Box>
+
+          <Box
+            sx={{
+              display: "flex",
+              alignItems: "center",
+            }}
+            marginTop={3}
+          >
+            <Rating
+              name="read-only"
+              value={4}
+              readOnly
+              precision={0.5}
+              size="small"
+            />
+            <Typography variant="body2" component="p" marginLeft={0.5}>
+              4.5
+            </Typography>
+            <Typography variant="body2" component="p" marginLeft={0.5}>
+              (655 reviews)
+            </Typography>
+          </Box>
+
+          <Box>
+            <Typography variant="h6" component="h3" marginLeft={0}>
+              From INR Rs25,000
+            </Typography>
+          </Box>
+        </Box>
       </Paper>
     </Grid>
   );
